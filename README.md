3168142a5205eae424a9a9d583d76b8d9657a2ca
Eugene-Fed <eugene.a.fed@gmail.com>
2024-02-12 19:41:42 +0300

    The first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# devman_chat-bot-01
+
+## Prestart config
+- Для вывода логов в `systemd` необходим пакет `python3-systemd`. Установка командой:  
+```
+apt update
+apt install python3-systemd
+```
+- Далее устанавливаем виртуальное окружение (при необходимости, любым удобным способом)  
+- и зависимости:  
+`pip install -r requirements.txt`
+
+## Start and Config
+- В корне необходимо скопировать файл `example.env` и вставить в ту же папку с именем `.env`.
+- Указать корректные токены в соответствующие переменные.  
+`DEVMAN_TOKEN` - токен пользователя API в Девман  
+`TG_BOT_TOKEN`- токен ТГ бота  
+`TG_CHAT_ID` - ID чата в личных сообщениях, в который бот будет отправлять оповещения  
+
+## Features
+- Получить имя пользователя:
+```
+updates = bot.get_updates()
+firts_name = updates[-1].message.from_user.first_name
+```
+- Используй файл `config/config.json` для настройки версии апи, задержек и текста отправляемых сообщений.
+
+## Config example
+```
+{
+  "urls":
+  {
+    "user_reviews": "https://dvmn.org/api/long_polling/",
+  },
+  "timeout": 120,
+  "sleep": 30,
+  "message": "Mentor has checked your work!\nTheme: {title}\nLink: {url}\n\n{review}",
+  "review_messages": ["Unfortunately there are errors in the work :(",
+    "Everything is fine! You can proceed to the next lesson :)"],
+}
+```
---
+++ main.py
@@ -0,0 +1,151 @@
+import time
+import requests
+import os
+import json
+import logging
+import telegram
+
+from telegram.ext import Updater, CommandHandler
+from dotenv import load_dotenv
+# from systemd.journal import JournalHandler    # для использования системных логов демона
+
+ROOT_DIR = os.path.dirname(__file__)
+CURRENCY_API_URL = "https://open.er-api.com/v6/latest/{cur}"
+
+def load_config():
+    '''Загрузка конфига происходит только первй раз при запуске демона, поэтому нет необходимости логировать ошибки'''
+    dotenv_path = os.path.join(ROOT_DIR, '.env')
+    config_path = os.path.join(ROOT_DIR, 'config', 'config.json')
+
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path)
+    if not os.environ.get('TG_BOT_TOKEN'):
+        raise EnvironmentError("You should set your `.env` file. Please check README.md.")
+    if os.path.exists(config_path):
+        return json.load(open(config_path, "r"))
+    else:
+        raise FileNotFoundError("File `config/config.json` not found. Please check README.md.")
+
+
+def start_callback(update, context):
+    print(context.args)
+    user_says = " ".join(context.args)
+    update.message.reply_text("User said: " + user_says)
+
+
+def help_callback(update, context):
+    print(context.args)
+    user_says = " ".join(context.args)
+    update.message.reply_text("How can I help you?")
+
+
+def convert_callback(update, context):
+    if len(context.args) != 4:
+        update.message.reply_text("To get currency convertation please write: \n"
+                                  "/convert <count> <currency_code> to <currency_code>\n\n"
+                                  "Example: /convert 100 USD to RUB")
+    else:
+        count_from, cur_from, _, cur_to = context.args
+
+        try:
+            response = requests.get(CURRENCY_API_URL.format(cur=cur_from))
+            response.raise_for_status()
+            currencies_info = response.json()
+            update.message.reply_text(currencies_info['rates'][cur_to] * float(count_from))
+        except ValueError:
+            update.message.reply_text(f"The firts param should be a number, not the: `{count_from}`")
+        except Exception as e:
+            update.message.reply_text(e)
+
+        print(count_from, cur_from, cur_to)
+
+
+def main():
+    logging.basicConfig(
+        filename=os.path.join(ROOT_DIR, 'log', 'app.log'),
+        filemode='a',
+        format='%(asctime)s - %(name)s - %(levelname)s: %(message)s',
+        datefmt='%d/%m/%Y %H:%M:%S',
+        level=logging.INFO
+    )
+    # systemd_log = logging.getLogger('systemd_log')      # для использования системных логов демона
+    # systemd_log.addHandler(JournalHandler())
+    # systemd_log.setLevel(logging.INFO)
+
+    config = load_config()
+    tg_bot_token = os.environ["TG_BOT_TOKEN"]
+    tg_chat_id = os.environ["TG_CHAT_ID"]
+
+    updater = Updater(token=tg_bot_token)
+    dispatcher = updater.dispatcher
+
+    # tg_bot = telegram.Bot(token=tg_bot_token)
+    currencies_url = config["urls"]["currencies_api"]
+
+
+    # systemd_log.info("Bot started")  # В журнал
+    logging.info("Bot started")  # В папку проекта
+    # tg_bot.send_message(text="Bot started", chat_id=tg_chat_id)  # В ТГ
+
+
+    ######################## Handlers ########################
+    start_handler = CommandHandler('start', start_callback)
+    dispatcher.add_handler(start_handler)
+
+    help_handler = CommandHandler('help', help_callback)
+    dispatcher.add_handler(help_handler)
+
+    convert_handler = CommandHandler('convert', convert_callback)
+    dispatcher.add_handler(convert_handler)
+
+    updater.start_polling()
+
+    # while True:
+    #     try:
+    #         response = requests.get(
+    #             reviews_url, headers=reviews_headers, timeout=config["timeout"], params=reviews_params
+    #         )
+    #         response.raise_for_status()
+    #         review_info = response.json()
+    #
+    #     except requests.exceptions.ReadTimeout:
+    #         continue
+    #     except requests.exceptions.ConnectionError:
+    #         logging.warning(f"Connection Error. Waiting connection for {config['sleep']} seconds.")
+    #         time.sleep(config["sleep"])
+    #         continue
+    #     except requests.exceptions.HTTPError as ex:
+    #         logging.error(f"Server returned: `{ex.response}`.")
+    #         break
+    #     except requests.exceptions.JSONDecodeError:
+    #         logging.error(f"There is no JSON in the server response. "
+    #                       f"Check if DEVMAN_TOKEN and url for user_reviews are correct.")
+    #         break
+    #
+    #     if ts := review_info.get("timestamp_to_request"):  # Проверка на Timeout сервера
+    #         reviews_params["timestamp"] = ts
+    #         continue
+    #
+    #     if attempts := review_info.get("new_attempts"):
+    #         for attempt in attempts:
+    #             should_be_rewrite = attempt.get("is_negative")
+    #             review_message = config.get("review_messages")[0] if should_be_rewrite \
+    #                 else config.get("review_messages")[1]
+    #             tg_bot.send_message(
+    #                 text=config.get("message").format(title=attempt.get("lesson_title"),
+    #                                                   url=attempt.get("lesson_url"),
+    #                                                   review=review_message),
+    #                 chat_id=tg_chat_id)
+    #             logging.info("Message sent")
+    #     if ts := review_info.get("last_attempt_timestamp"):  # Поиск таймстампа в ответе с инфой.
+    #         reviews_params["timestamp"] = ts
+
+
+if __name__ == '__main__':
+    main()
+
+    response = requests.get('https://open.er-api.com/v6/latest/RUB')
+
+    print(response.json())
+
+
