7c66821904caa14f09fe579f7ee06c2b36dec079
Eugene-Fed <eugene.a.fed@gmail.com>
2024-02-12 22:11:58 +0300

    Main work is ready

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import requests
 import os
@@ -13,7 +14,6 @@ ROOT_DIR = os.path.dirname(__file__)
 CURRENCY_API_URL = "https://open.er-api.com/v6/latest/{cur}"
 
 def load_config():
-    '''Загрузка конфига происходит только первй раз при запуске демона, поэтому нет необходимости логировать ошибки'''
     dotenv_path = os.path.join(ROOT_DIR, '.env')
     config_path = os.path.join(ROOT_DIR, 'config', 'config.json')
 
@@ -21,6 +21,7 @@ def load_config():
         load_dotenv(dotenv_path)
     if not os.environ.get('TG_BOT_TOKEN'):
         raise EnvironmentError("You should set your `.env` file. Please check README.md.")
+
     if os.path.exists(config_path):
         return json.load(open(config_path, "r"))
     else:
@@ -51,13 +52,25 @@ def convert_callback(update, context):
             response = requests.get(CURRENCY_API_URL.format(cur=cur_from))
             response.raise_for_status()
             currencies_info = response.json()
-            update.message.reply_text(currencies_info['rates'][cur_to] * float(count_from))
+            update.message.reply_text(currencies_info['rates'][cur_to.upper()] * float(count_from))
         except ValueError:
             update.message.reply_text(f"The firts param should be a number, not the: `{count_from}`")
-        except Exception as e:
-            update.message.reply_text(e)
-
-        print(count_from, cur_from, cur_to)
+        except KeyError:
+            # Чтобы не париться с отдельной проверкой первого и второго параметра по отдельности
+            # TODO - реализовать вывод полного списка доступных кодов
+            update.message.reply_text(f"There is no currency with code: `{cur_from.upper()}` or `{cur_to.upper()}`")
+        except requests.exceptions.ConnectionError:
+            logging.warning(f"Connection Error.")
+            update.message.reply_text(f"Some troubles with connection. Wait a minute, please.")
+        except requests.exceptions.HTTPError as ex:
+            logging.error(f"Server returned: `{ex.response}`.")
+            update.message.reply_text(f"Some troubles with Currencies Server.")
+        except requests.exceptions.JSONDecodeError as ex:
+            logging.error(f"There is no JSON in the server response:\n{str(ex)}")
+            update.message.reply_text(f"Some troubles with Currencies Server.")
+        except Exception as ex:
+            logging.error(f"There is an unexpected error: \n{str(ex)}")
+            update.message.reply_text(f"Houston, we have problems bip-bop-bop...")
 
 
 def main():
@@ -72,21 +85,24 @@ def main():
     # systemd_log.addHandler(JournalHandler())
     # systemd_log.setLevel(logging.INFO)
 
-    config = load_config()
+    try:
+        config = load_config()
+
+    except EnvironmentError as ex:
+        logging.error(f"{str(ex)}.")
+        sys.exit()
+
+    except FileNotFoundError as ex:
+        logging.error(f"{str(ex)}.")
+        sys.exit()
+
     tg_bot_token = os.environ["TG_BOT_TOKEN"]
-    tg_chat_id = os.environ["TG_CHAT_ID"]
 
     updater = Updater(token=tg_bot_token)
     dispatcher = updater.dispatcher
 
-    # tg_bot = telegram.Bot(token=tg_bot_token)
-    currencies_url = config["urls"]["currencies_api"]
-
-
     # systemd_log.info("Bot started")  # В журнал
     logging.info("Bot started")  # В папку проекта
-    # tg_bot.send_message(text="Bot started", chat_id=tg_chat_id)  # В ТГ
-
 
     ######################## Handlers ########################
     start_handler = CommandHandler('start', start_callback)
@@ -143,9 +159,5 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-    response = requests.get('https://open.er-api.com/v6/latest/RUB')
-
-    print(response.json())
 
 
